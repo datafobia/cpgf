9acecfd6a7a27e57bb6b0736e64dd4a8fcbb398b
Denisson Silva <denisson@denissonsilva.com>
2025-06-08 17:26:01 -0300

    get_cpgf

--- R/cpgf_data.R
+++ R/cpgf_data.R
@@ -1,11 +1,29 @@
-# #' Dados da CPGF
-# #'
-# #' Descrição detalhada dos dados aqui
-# #'
-# #' @format Um data frame com X linhas e Y colunas:
-# #' \describe{
-# #'   \item{col1}{Descrição}
-# #'   \item{col2}{Descrição}
-# #' }
-# #' @source Fonte dos dados
+#' Dados da CPGF
+#'
+#' Descrição detalhada dos dados aqui
+#'
+#' @format Um data frame com X linhas e 21 colunas:
+#' \describe{
+#'   \item{codigo_orgao_superior}{Descrição}
+#'   \item{nome_orgao_superior}{Descrição}
+#'   \item{codigo_orgao}{Descrição}
+#'   \item{nome_orgao}{Descrição}
+#'   \item{codigo_unidade_gestora}{Descrição}
+#'   \item{nome_unidade_gestora}{Descrição}
+#'   \item{ano_extrato}{Descrição}
+#'   \item{mes_extrato}{Descrição}
+#'   \item{cpf_portador}{Descrição}
+#'   \item{nome_portador}{Descrição}
+#'   \item{cnpj_ou_cpf_favorecido}{Descrição}
+#'   \item{nome_favorecido}{Descrição}
+#'   \item{transacao}{Descrição}
+#'   \item{data_transacao}{Descrição}
+#'   \item{valor_transacao}{Descrição}
+#'   \item{data_date}{Descrição}
+#'   \item{ano_mes_dia}{Descrição}
+#'   \item{ano_mes}{Descrição}
+#'   \item{valor_transacao2}{Descrição}
+#'   \item{valor_transacao_deflacionado}{Descrição}
+#' }
+#' @source Fonte dos dados
 # "cpgf_data"
---
+++ R/get_cpgf.R
@@ -0,0 +1,33 @@
+#' Download da CPGF
+#'
+#' @param year. Valid options are 2013 to corrent.
+#'
+#' @param month Valid options are 01 a 12.
+#'
+#' @return \code{(get_cpgf)} returns a \code{data.frame}
+
+get_cpgf <- function(year, month){
+
+  # padonizando o mes com dois numeros
+  month <- stringr::str_pad(month, 2, "left", "0")
+
+  # download dos dados
+  dados <- data.table::fread(paste0("https://portaldatransparencia.gov.br/download-de-dados/cpgf/",
+                                    year, month),
+                             encoding = "Latin-1")
+
+  #padronização dos nomes das variaveis
+  names(dados) <- c("codigo_orgao_superior", "nome_orgao_superior",
+                    "codigo_orgao", "nome_orgao",
+                    "codigo_unidade_gestora", "nome_unidade_gestora",
+                    "ano_extrato", "mes_extrato",
+                    "cpf_portador", "nome_portador",
+                    "cnpj_ou_cpf_favorecido", "nome_favorecido",
+                    "transacao", "data_transacao", "valor_transacao")
+
+  return(dados)
+}
+
+
+
+
